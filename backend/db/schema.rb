680b681fcbcbe241c7a29e4e4f29a5d39dcdc5a3
Adan Gonzalez <adangrx@gmail.com>
2025-01-29 14:10:14 +0000

    feat: update schema after run migrations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_29_135805) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_29_140152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,11 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_29_135805) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_parts_on_name", unique: true
+  end
+
+  create_table "product_parts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
