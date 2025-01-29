35b05ff6fb86543a4923f909dd80350d30a5251c
Adan Gonzalez <adangrx@gmail.com>
2025-01-29 10:54:51 +0000

    chore: add factory bot settings to rspec

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -21,6 +21,9 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
+  # Factory bot
+  config.include FactoryBot::Syntax::Methods
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
