b46f2c1fe827e110e510f185527f0cc4e3213e8d
Adan Gonzalez <adangrx@gmail.com>
2025-01-30 13:48:28 +0000

    feat: added restriction relationships

--- backend/app/models/part_option.rb
+++ backend/app/models/part_option.rb
@@ -1,10 +1,17 @@
 class PartOption < ApplicationRecord
   belongs_to :part
+  has_many :pricing_rules, foreign_key: :part_option_id, dependent: :destroy
+  has_many :dependent_pricing_rules, class_name: 'PricingRule', foreign_key: :dependent_part_option_id, dependent: :destroy
+
   has_many :source_restrictions, class_name: 'Restriction', foreign_key: :source_part_option_id, dependent: :destroy
   has_many :target_restrictions, class_name: 'Restriction', foreign_key: :target_part_option_id, dependent: :destroy
 
   validates :name, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :in_stock, inclusion: { in: [true, false] }
+
+  def restrictions
+    Restriction.where("source_part_option_id = :id OR target_part_option_id = :id", id: self.id)
+  end
 
 end
--- backend/app/models/restriction.rb
+++ backend/app/models/restriction.rb
@@ -3,4 +3,12 @@ class Restriction < ApplicationRecord
   belongs_to :target_part_option, class_name: 'PartOption'
 
   validates :restriction_type, inclusion: { in: %w[requires excludes] }
+
+  def excludes?
+    self.restriction_type == 'excludes'
+  end
+
+  def requires?
+    self.restriction_type == 'requires'
+  end
 end
